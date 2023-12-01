913ce587c69208fa2af01ee0618384f3495b240d
Pedro-Henriquebg7 <85884290+Pedro-Henriquebg7@users.noreply.github.com>
2023-12-01 15:47:01 -0300

    Delete academia directory

--- academia/alunos_app/forms.py
+++
@@ -1,9 +0,0 @@
-# alunos_app/forms.py
-
-from django import forms
-from .models import Aluno
-
-class AlunoForm(forms.ModelForm):
-    class Meta:
-        model = Aluno
-        fields = ['nome', 'idade']
