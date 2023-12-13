9b42b2c76a5ec77b0ce8bd135ec0abce8547b2ea
Pedro-Henriquebg7 <85884290+Pedro-Henriquebg7@users.noreply.github.com>
2023-12-13 10:28:38 -0300

    Add files via upload

--- Academia/academia/urls.py
+++ Academia/academia/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from alunos_app.views import AlunoListView  # Importe a classe AlunoListView
+from alunos_app.views import pagina_inicial # Importe a classe AlunoListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('alunos_app/', include('alunos_app.urls')),
     # Adicione a seguinte linha para a URL raiz:
-    path('', AlunoListView.as_view(), name='lista_alunos_root'),  # Use a classe AlunoListView para a URL raiz
+    path('', pagina_inicial, name='alunos_app/pagina_inicial'),  # Use a classe AlunoListView para a URL raiz
 ]
 
